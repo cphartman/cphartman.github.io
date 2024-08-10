ef8d0d534eb59428bab00c7a3ee9a1c4fa3b65c2
Christopher Hartman <cphartman@gmail.com>
2024-08-10 12:44:26 -0400

    update

--- projects/parrot/js/debug.js
+++ projects/parrot/js/debug.js
@@ -21,13 +21,16 @@ function TestMicrophone() {
   recognition.interimResults = true;
   recognition.lang = "en-us";
   recognition.onstart = function () {
-      recognition.stop();
+    Log("Mic Started");  
+    recognition.stop();
+  };
+  recognition.onend = function () {
+    Log("Mic Ended");  
   };
   recognition.start();
-
 }
 
-function TestSpeaker() {
+function TestTTS() {
   let utterance = new SpeechSynthesisUtterance("Rock! Rock!");
   utterance.volume = 50;
   utterance.pitch = 2;
@@ -35,4 +38,34 @@ function TestSpeaker() {
   utterance.voice = GetVoiceByUri("com.apple.eloquence.en-US.Shelley");
   utterance.pitch = 2;
   speechSynthesis.speak(utterance);
+}
+
+function TestSpeaker() {
+  navigator.mediaDevices.enumerateDevices()
+  .then(devices => {
+    devices.forEach(device => {
+      if (device.kind === 'audiooutput' || device.kind === 'audioinput')  {
+        Log(JSON.stringify(device));
+      }
+    });
+  })
+  .catch(err => {
+    Log('Error enumerating devices:', err);
+  });
+}
+
+function SelectOutput() {
+  navigator.mediaDevices
+    .selectAudioOutput()
+    .then((device) => {
+      Log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+    })
+    .catch((err) => {
+      Log(`Error: ${err.name}: ${err.message}`);
+    });
+}
+
+function TestAudioOutput() {
+  let audioEl = document.querySelector("#debug-test-audio");
+  audioEl.play();
 }
